55cd75568236943e3835d23d738360d585f11b36
João Nogueira <joao.nogueira@datarisk.io>
2023-03-03 11:51:20 -0300

    info about column names

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import os
 import openai
+import logging
 import pandas as pd
 import streamlit as st
 
@@ -7,8 +8,10 @@ import db_utils
 import openai_utils
 
 st.title("Faça Consultas SQL em Português")
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
+st.info("Certifique-se de que o nome das colunas não contenha espaços ou caracteres especiais.")
 uploaded_file = st.file_uploader("Faça upload do arquivo csv", type=".csv", accept_multiple_files=False)
 
 if uploaded_file is None:
@@ -35,9 +38,11 @@ with st.form("query_form"):
         database = db_utils.dataframe_to_database(df, table_name=tb_name)
         fixed_sql_prompt = openai_utils.create_table_definition_prompt(df, table_name=tb_name)
         final_prompt = openai_utils.combine_prompts(fixed_sql_prompt, user_input)
+        logging.info(f"Final Prompt: {final_prompt}")
         response = openai_utils.send_to_openai(final_prompt)
         proposed_query = response["choices"][0]["text"]
         proposed_query_postprocessed = db_utils.handle_response(response)
+        logging.info(f"Response obtained. Proposed sql query: {proposed_query_postprocessed}")
         result = db_utils.execute_query(database, proposed_query_postprocessed, return_pandas=True)
         st.write(result)
 
