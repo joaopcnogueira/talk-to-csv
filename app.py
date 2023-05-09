bd76610ed417ac51e3ccda69f432a3cd386799bd
João Nogueira <joao.nogueira@datarisk.io>
2023-05-09 19:42:10 -0300

    refactor to langchain

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import os
 import openai
-import logging
 import pandas as pd
 import streamlit as st
 
-import db_utils
-import openai_utils
+from langchain.llms import OpenAI
+from langchain.agents import create_pandas_dataframe_agent
+
 
 st.title("Faça Consultas SQL em Português")
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 st.info("Certifique-se de que o nome das colunas não contenha espaços ou caracteres especiais.")
@@ -35,15 +34,6 @@ with st.form("query_form"):
 
    submitted = st.form_submit_button("Submit")
    if submitted:
-        database = db_utils.dataframe_to_database(df, table_name=tb_name)
-        fixed_sql_prompt = openai_utils.create_table_definition_prompt(df, table_name=tb_name)
-        final_prompt = openai_utils.combine_prompts(fixed_sql_prompt, user_input)
-        logging.info(f"Final Prompt: {final_prompt}")
-        response = openai_utils.send_to_openai(final_prompt)
-        proposed_query = response["choices"][0]["text"]
-        proposed_query_postprocessed = db_utils.handle_response(response)
-        logging.info(f"Response obtained. Proposed sql query: {proposed_query_postprocessed}")
-        result = db_utils.execute_query(database, proposed_query_postprocessed, return_pandas=True)
+        agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=True)       
+        result = agent.run(user_input)
         st.write(result)
-
-
